842c7d52525ba67feaf12dd60606a4b5c7e4a2a4
Nicholas <nicholas0295@gmail.com>
2023-04-27 19:25:34 +0800

    Renamed Methods, added Timers and TODOs,

--- src/pong.cpp
+++ src/pong.cpp
@@ -1,16 +1,18 @@
 #include "pong.hpp"
 #include <SFML/Graphics/Rect.hpp>
+#include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <iostream>
 
 const sf::Vector2i DEFAULT_WIN_DIMS{1366, 768};
 const int DEFAULT_FRAME_LIMIT = 120;
 
 Pong::Pong()
-    : m_window(sf::VideoMode(DEFAULT_WIN_DIMS.x, DEFAULT_WIN_DIMS.y),
-               "Pong Game"),
-      m_camera(static_cast<sf::Vector2f>(DEFAULT_WIN_DIMS / 2),
-               static_cast<sf::Vector2f>(DEFAULT_WIN_DIMS))
+    : m_window(sf::VideoMode(DEFAULT_WIN_DIMS.x, DEFAULT_WIN_DIMS.y), "Pong Game"),
+      m_camera(static_cast<sf::Vector2f>(DEFAULT_WIN_DIMS / 2), static_cast<sf::Vector2f>(DEFAULT_WIN_DIMS)),
+      m_clock(sf::Clock()), m_previous_time(), m_dt(0.0f)
 {
     m_window.setFramerateLimit(DEFAULT_FRAME_LIMIT);
 }
@@ -20,41 +22,57 @@ void Pong::run()
 {
     while (m_window.isOpen())
     {
+        // Updates m_dt for use in physics / rendering purposes.
+        // Must be called first
+        updateDeltaTime();
+
         // Check for any inputs (polling events)
         sf::Event event;
-        handleEvents(event);
+        while (m_window.pollEvent(event))
+        {
+            handleCommonEvents(event);
+            // TODO: add StateManager->handleStateSpecificEvents();
+        }
 
-        // Update logic of the program
-        updateGameLogic();
+        // Update logic of the program.
+        updateCommonLogic(m_dt);
+        // TODO: add StateManager->updateStateSpecificLogic(m_dt);
 
         // Rendering
         m_window.clear();
 
         // draw() Commands Here:
+        // TODO: add StateManager->drawStateSpecificItems();
 
         // Display the whatever you have drawn after m_window.clear();
         m_window.display();
     }
 }
 
-void Pong::updateGameLogic() {}
-
-void Pong::handleEvents(sf::Event &event)
+void Pong::updateDeltaTime()
 {
-    // We put this in a while loop to ensure that the events are polled
-    while (m_window.pollEvent(event))
+    sf::Time current_time = m_clock.getElapsedTime();
+    m_dt = (current_time - m_previous_time).asSeconds();
+    m_previous_time = current_time;
+}
+
+void Pong::updateCommonLogic(const float &dt){};
+
+
+
+void Pong::handleCommonEvents(const sf::Event &event)
+{
+
+    if (event.type == sf::Event::Closed)
     {
-        if (event.type == sf::Event::Closed)
+        m_window.close();
+    }
+
+    if (event.type == sf::Event::KeyPressed)
+    {
+        if (event.key.code == sf::Keyboard::Escape)
         {
             m_window.close();
-        }
-
-        if (event.type == sf::Event::KeyPressed)
-        {
-            if (event.key.code == sf::Keyboard::Escape)
-            {
-                m_window.close();
-            }
         }
     }
 }
